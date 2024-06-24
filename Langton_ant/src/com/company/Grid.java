29d64cbe25b984c3b47820a52f85d33c561ad71f
Zampagnone <veroneslorenzo@gmail.com>
2024-06-24 20:54:36 +0200

    implemented method display for class Grid.java

--- Langton_ant/src/com/company/Grid.java
+++ Langton_ant/src/com/company/Grid.java
@@ -1,23 +1,42 @@
 package com.company;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Grid extends JFrame{
     private int size = 10;
+    JFrame frame = new JFrame();
 
     private JButton [][] grid;
 
     public Grid(int size) {
         this.size = size;
 
-        JFrame frame = new JFrame();
+
 
         this.grid = new JButton[size][size];
 
         for(int i = 0; i < size; i++){
             for(int j = 0; j < size; j++){
-                grid[i][j] = new JButton(String.valueOf((char)(i+41)) + (j));
+                grid[i][j] = new JButton(" ");
             }
         }
+    }
+    public void display(){
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(size, size));
+
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                panel.add(grid[i][j]);
+            }
+        }
+
+        frame.add(panel, BorderLayout.CENTER);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Langton's ant");
+        frame.pack();
+        frame.setVisible(true);
     }
 }
--- Langton_ant/src/com/company/Main.java
+++ Langton_ant/src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Grid grid = new Grid(10);
+
+        grid.display();
     }
 }
